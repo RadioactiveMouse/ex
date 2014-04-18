6b7dd925166bfa41f7ccbfb175eea17634dd2127
Laura McCormack <laura.mccormack227@gmail.com>
2014-04-18 19:10:06 +0100

    add refute method and rename file

---
+++ ex.go
@@ -0,0 +1,18 @@
+package ex
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Pect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func Refute(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
