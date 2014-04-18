c9c5583d14669a6ea9ee0522537d4b1a6d6e9cd0
Tom Townsend <tom.towny@gmail.com>
2014-04-18 17:27:42 +0100

    fix unexported field

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func pect(t *testing.T, a interface{}, b interface{}) {
+func Pect(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		t.Errorf("Expected %v (type %v) - Got %v (type %v)",b, reflect.TypeOf(b), a, reflect.TypeOf(a))
 	}
